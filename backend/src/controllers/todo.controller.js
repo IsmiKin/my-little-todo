0a59cd96367cf891281f0d21533c8b923e4338be
Ismael Ors <ismikin@gmail.com>
2019-01-29 19:05:19 +0100

    CRUD actions

--- backend/src/controllers/todo.controller.js
+++ backend/src/controllers/todo.controller.js
@@ -13,19 +13,106 @@ const findAll = (req, res) => {
 }
 
 const createNew = (req, res) => {
-  console.log('calling create endpoint')
+  if (!req.body.name) {
+    return res.status(400).send({
+      message: 'Todo name can not be empty'
+    })
+  }
+
+  const todo = new Todo({
+    name: req.body.name,
+    done: req.body.done
+  })
+
+  todo
+    .save()
+    .then(data => {
+      res.send(data)
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: err.message || 'Some error occurred while creating the Todo.'
+      })
+    })
 }
 
 const findOne = (req, res) => {
-  console.log('calling find one todo')
+  Todo.findById(req.params.todoId)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send({
+          message: 'Todo not found with id ' + req.params.todoId
+        })
+      }
+      res.send(todo)
+    })
+    .catch(err => {
+      if (err.kind === 'ObjectId') {
+        return res.status(404).send({
+          message: 'Todo not found with id ' + req.params.todoId
+        })
+      }
+      return res.status(500).send({
+        message: 'Error retrieving todo with id ' + req.params.todoId
+      })
+    })
 }
 
 const update = (req, res) => {
-  console.log('calling find one todo')
+  if (!req.body.name) {
+    return res.status(400).send({
+      message: 'Todo name can not be empty'
+    })
+  }
+
+  Todo.findByIdAndUpdate(
+    req.params.todoId,
+    {
+      name: req.body.name,
+      done: req.body.done || false
+    },
+    { new: true }
+  )
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send({
+          message: 'Todo not found with id ' + req.params.todoId
+        })
+      }
+      res.send(todo)
+    })
+    .catch(err => {
+      if (err.kind === 'ObjectId') {
+        return res.status(404).send({
+          message: 'Todo not found with id ' + req.params.todoId
+        })
+      }
+      return res.status(500).send({
+        message: 'Error updating todo with id ' + req.params.todoId
+      })
+    })
 }
 
 const remove = (req, res) => {
-  console.log('calling delete one todo')
+  Todo.findByIdAndRemove(req.params.todoId)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send({
+          message: 'Todo not found with id ' + req.params.todoId
+        })
+      }
+      res.send({ message: 'Todo deleted successfully!' })
+    })
+    .catch(err => {
+      if (err.kind === 'ObjectId' || err.name === 'NotFound') {
+        return res.status(404).send({
+          message: 'Todo not found with id ' + req.params.todoId
+        })
+      }
+      return res.status(500).send({
+        message: 'Could not delete todo with id ' + req.params.todoId
+      })
+    })
 }
 
 module.exports = {
